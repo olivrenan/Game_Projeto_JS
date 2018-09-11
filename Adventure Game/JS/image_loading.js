65ec1cca1bfb4530b28f82549c280ab7eeebccb7
Renan Oliveira <ren.moura.oliveira@gmail.com>
2018-09-11 12:07:08 -0300

    Atualizações finais

--- Adventure Game/JS/image_loading.js
+++ Adventure Game/JS/image_loading.js
@@ -1,44 +1,44 @@
-var warriorPic = document.createElement('img');
-
+var warriorPic = document.createElement("img");
 var worldPics = [];
 
-var picsToLoad = 0;
+var picsToLoad = 0; // set automatically based on imageList in loadImages()
 
 function countLoadedImagesAndLaunchIfReady() {
-    picsToLoad--;
-    if (picsToLoad == 0) {
-        imageLoadingDoneSoStartGame();
-    }
+	picsToLoad--;
+	console.log(picsToLoad);
+	if(picsToLoad == 0) {
+		imageLoadingDoneSoStartGame();
+	}
 }
 
 function beginLoadingImage(imgVar, fileName) {
-    imgVar.onload = countLoadedImagesAndLaunchIfReady;
-    imgVar.src = 'Images/' + fileName;
+	imgVar.onload = countLoadedImagesAndLaunchIfReady;
+	imgVar.src = "images/"+fileName;
 }
 
-function loadImagesForWorldCode(worldCode, fileName) {
-    worldPics[worldCode] = document.createElement('img');
-    beginLoadingImage(worldPics[worldCode],fileName);
+function loadImageForWorldCode(worldCode, fileName) {
+	worldPics[worldCode] = document.createElement("img");
+	beginLoadingImage(worldPics[worldCode], fileName);
 }
 
 function loadImages() {
-    var imageList = [
-        {varName: warriorPic, theFile: 'warrior.png'},
-        {worldType: WORLD_ROAD, theFile: 'world_road.png'},
-        {worldType: WORLD_WALL, theFile: 'world_wall.png'},
-        {worldType: WORLD_GOAL, theFile: 'world_goal.png'},
-        {worldType: WORLD_TREE, theFile: 'world_tree.png'},
-        {worldType: WORLD_FLAG, theFile: 'world_flag.png'}
-    ];
+	var imageList = [
+		{varName: warriorPic, theFile: "warrior.png"},
 
-    picsToLoad = imageList.length;
+		{worldType: TILE_GROUND, theFile: "world_ground.png"},
+		{worldType: TILE_WALL, theFile: "world_wall.png"},
+		{worldType: TILE_GOAL, theFile: "world_goal.png"},
+		{worldType: TILE_KEY, theFile: "world_key.png"},
+		{worldType: TILE_DOOR, theFile: "world_door.png"}
+		];
 
-    for (var i=0; i<imageList.length; i++) {
-        if (imageList[i].varName != undefined) {
-            beginLoadingImage(imageList[i].varName, imageList[i].theFile);
-        }
-        else {
-            loadImagesForWorldCode(imageList[i].worldType, imageList[i].theFile);
-        }
-    }
+	picsToLoad = imageList.length;
+
+	for(var i=0;i<imageList.length;i++) {
+		if(imageList[i].varName != undefined) {
+			beginLoadingImage(imageList[i].varName, imageList[i].theFile);
+		} else {
+			loadImageForWorldCode(imageList[i].worldType, imageList[i].theFile);
+		}
+	}
 }
--- Adventure Game/JS/main.js
+++ Adventure Game/JS/main.js
@@ -3,39 +3,39 @@ var canvas, canvasContext;
 var blueWarrior = new warriorClass();
 
 window.onload = function() {
-    canvas = document.getElementById('gameCanvas');
-    canvasContext = canvas.getContext('2d');
+	canvas = document.getElementById('gameCanvas');
+	canvasContext = canvas.getContext('2d');
 
-    colorRect(0,0, canvas.width,canvas.height, 'black');
-    colorText('LOADING IMAGES', canvas.width/2,canvas.height/2, 'white');
+	colorRect(0,0, canvas.width,canvas.height, 'black');
+	colorText("LOADING IMAGES", canvas.width/2, canvas.height/2, 'white');
 
-    loadImages();
+	loadImages();
 }
 
 function imageLoadingDoneSoStartGame() {
-    var framesPerSecond = 30;
-    setInterval(updateAll, 1000/framesPerSecond);   
+	var framesPerSecond = 30;
+	setInterval(updateAll, 1000/framesPerSecond);
 
-    setupInput();
+	setupInput();
 
-    loadLevel(levelOne);
+	loadLevel(levelOne);
 }
 
 function loadLevel(whichLevel) {
-    worldGrid = whichLevel.slice();
-    blueWarrior.reset(warriorPic, 'Blue Storm');
+	worldGrid = whichLevel.slice();
+	blueWarrior.reset(warriorPic, "Blue Storm");
 }
 
 function updateAll() {
-    moveAll();
-    drawAll();
+	moveAll();
+	drawAll();
 }
 
 function moveAll() {
-    blueWarrior.move();
+	blueWarrior.move();
 }
 
 function drawAll() {
-    drawWorlds();
-    blueWarrior.draw();    
-}
+	drawWorld();
+	blueWarrior.draw();
+} 
